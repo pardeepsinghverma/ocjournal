b2d6471e26d1c22cc13019ca1951ceb8b5e20025
Pardeep Verma <pardeep@fathershops.com>
2024-11-05 00:57:12 +0530

    added redux toolkit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './navigation/DrawerNavigator';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <DrawerNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
+//;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,18 @@
 // src/screens/HomeScreen.js
 import React from 'react';
 import { Text } from 'react-native';
-import { View } from 'tamagui';
+import { Button, View } from 'tamagui';
 import LayoutRenderer from '../modules/layoutRenderer';
+import { useDispatch, useSelector } from 'react-redux';
+import { setData } from '../store/dataSlice';
 
 export default function HomeScreen() {
+  const { currentSubDomain, extra } = useSelector(state => state.data);
+  const dispatch = useDispatch();
+  const clickHanddler = () => {
+    dispatch(setData({ name: "extra", data: "anmol111" }));
+    console.log(extra);
+  }
   // const [layoutData, setLayoutData] = useState(null);
 
   // useEffect(() => {
@@ -21,9 +29,11 @@ export default function HomeScreen() {
   //   fetchLayout();
   // }, []);
 
+  console.log(currentSubDomain);
   return (
     <View style={{ flex: 1 }}>
       {/* {layoutData ? <LayoutRenderer layoutData={layoutData} /> : <ActivityIndicator size="large" />} */}
+      <Button title="Click me" color={'#000000'} variant='outlined' onPress={clickHanddler} />
       <LayoutRenderer />
     </View>
   );
